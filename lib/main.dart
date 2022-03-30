08dc598997c75a58f5406f7ec0dac5f5528a51bc
runenisbeth <rune-nisbeth@hotmail.com>
2022-03-30 12:03:15 +0200

    added firebase database and upload weather data when opening the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCIwHtOPFKy_jHrArTxlzcjWBar-TeeI_c",
+      appId: "1:275694460331:web:c97b401267238acb8b86ee",
+      messagingSenderId: "275694460331",
+      projectId: "moodrise-dtu",
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -32,6 +45,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePageWidget extends StatefulWidget {
+  // READ FILE FROM FIREBASE
+  //final Stream<QuerySnapshot> moods = FirebaseFirestore.instance.collection('moods').snapshots();
   const HomePageWidget();
 
   @override
@@ -46,6 +61,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04
 // &exclude=current,minutely,hourly,alerts&appid=47dced083ea25cf48cf2d5a4c6d50e4c
 // lat=55.68&lon=12.59 ≈ Nyhavn
+  var date;
   var tempt;
   var clouds;
   var uvi;
@@ -57,11 +73,18 @@ Future getWeather () async {
   http.Response response = await http.get(weatherComURL);
   var result = jsonDecode(response.body);
   setState((){
+    this.date = result["daily"][0]['dt'];
     this.clouds = result["daily"][0]['clouds'];
     this.uvi = result["daily"][0]['uvi'];
     this.sunrise = result["daily"][0]['sunrise'];
     this.sunset = result["daily"][0]['sunset'];
     this.tempt =  result["daily"][0]['temp']['day'];
+    // UPLOAD WEATHER DATA TO FIREBASE 
+    CollectionReference moods = FirebaseFirestore.instance.collection('moods');
+    moods
+      .add({'date':date, 'clouds': clouds, 'tempt':tempt, 'sunrise':sunrise, 'sunset':sunset, 'uvi':uvi})
+      .then((value) => print('Mood added'))
+      .catchError((error) => print('Failed to add mood : $error'));
     // PRINT WEATHER DATA TO CONSOLE
     print("clouds");
     print(clouds);
